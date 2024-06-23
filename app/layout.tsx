c870d7342b4c22b8ab67a193d7221b439628047e
Shiro <n0c0den0life@no.com>
2024-06-23 06:34:15 -0400

    beep boop

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
+  title: 'Code Fridays',
+  description: 'Code Fridays is a platform for aspiring developers.',
   icons: {
     icon: '/assets/images/logo.svg'
   }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -10,9 +10,9 @@ const Header = () => {
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
-          <Image 
+          <Image
             src="/assets/images/logo.svg" width={128} height={38}
-            alt="Evently logo" 
+            alt="CodeFridays logo"
           />
         </Link>
 
